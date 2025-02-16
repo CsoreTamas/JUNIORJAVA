5dd2459958159b663e5e9fe061c4470103568073
Lángi Fruzsina <langifruzsina@Langi-MacBook-Air.local>
2025-02-16 10:49:50 +0100

    first project

---
+++ Vers/src/Poem.java
@@ -0,0 +1,46 @@
+
+public class Poem {
+    public static void main(String[] args) {
+
+
+        System.out.println("Petőfi István" + "\n");
+        System.out.println("Sándor Bátyámhoz" + "\n");
+        System.out.println(
+                """
+                        Igérsz nekem paripát,
+                        Még pedig párjával;
+                        Legyenek bármily drágák,
+                        Nem gondolsz árával.
+                        
+                        Csak hogy Pisti öcsédnek
+                        Nagyobb kedve legyen,
+                        Mert eztán vásárokra
+                        Majd lóháton megyen.
+                        
+                        Jól meggondold bátyám,
+                        Amit ajánlottál,
+                        Tudod, hogy az ígéret
+                        Adomány után vár.
+                        
+                        Igyekezz hát, hogy gazdag
+                        Minél előbb legyél,
+                        Istók öcséd számára
+                        Paripákat vegyél.
+                        
+                        Attól tartok, hogyha majd
+                        Gazdagságod várom,
+                        A vásárra holtomig
+                        Gyalog kell sétálnom.
+                        """);
+
+        /*
+        1. Google keresés
+        2. Vers, költő, cím kimásolása
+        3. Vers, költő, cím beillesztése System.out.prinln-be
+        4. Író, cím és a vers között hézag hagyása
+        5. Java javaslatára "text block" használata
+        5. Program futtatása
+         */
+
+    }
+}
