f7a05239876c2d77c057dc86ae05bcde24aa095f
CsoreTamas <csore.tamas123@gmail.com>
2025-03-01 11:07:22 +0100

    Alternatingcalculations

--- Alternatingcalculations/src/Task8.java
+++ Alternatingcalculations/src/Task8.java
@@ -6,20 +6,20 @@ public class Task8 {
 
         for (int i = 0; i < numbers.length; i++) {
             // azért indulunk i = 0-ról, mert  a 0 a tömb indexét jelzi!!!
-            int currentlyNumber = numbers[i];
+            int currentNumber = numbers[i];
             // numbers[] tömb i-edig elemét elmentünk egy változóba.
             boolean isPrime = true;
-            for (int j = 2; j <= Math.sqrt(currentlyNumber); j++) {
+            for (int j = 2; j <= Math.sqrt(currentNumber); j++) {
                 //azért j = 2, mert 0-val és 1-el nem érdemes ellenőrizni
-                if (currentlyNumber % j == 0) {
+                if (currentNumber % j == 0) {
                     isPrime = false;
                     break;
                 }
             }
             if (isPrime) {
-                numbersSum += currentlyNumber * currentlyNumber;
+                numbersSum += currentNumber * currentNumber;
             } else {
-                numbersSum += currentlyNumber;
+                numbersSum += currentNumber;
             }
         }
         System.out.println(numbersSum);
