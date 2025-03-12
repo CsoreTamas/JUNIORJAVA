953cc25d1d80d59bd6eaafc7e5ab1a7e4fc7aa84
CsoreTamas <csore.tamas123@gmail.com>
2025-03-12 12:23:36 +0100

    tip-calculator

--- tip-calculator/src/TipAmountTest.java
+++ tip-calculator/src/TipAmountTest.java
@@ -5,23 +5,79 @@ public class TipAmountTest {
     @Test
     void shouldReturnSumOfTheArray() {
         //given
-        int[] array = {2000, 1000, 500, 500};
+        int[] array = {25, 25, 25, 25};
         //when
         int result = TipCalculator.calculateCosts(array);
         //than
-        Assertions.assertEquals(4000, result);
+        Assertions.assertEquals(100, result);
     }
 
     @Test
-    void shouldReturnTotalCostAndTipPercentageResult() {
+    void tenPercentShouldReturnTen() {
         //given
-        int[] totalCost = {2000, 1000, 500, 500};
-        int tip = 20;
+        int[] totalCost = {25, 25, 25, 25};
+        int tipPercentage = 10;
         //when
-        int result = TipCalculator.calculateTipAmount(totalCost, tip);
+        int result = TipCalculator.calculateTipAmount(totalCost, tipPercentage);
         //than
-        Assertions.assertEquals(800, result);
+        Assertions.assertEquals(10, result);
+    }
+
+    @Test
+    void eightyPercentShouldReturnEighty() {
+        //given
+        int[] totalCost = {25, 25, 25, 25};
+        int tipPercentage = 80;
+        //when
+        int result = TipCalculator.calculateTipAmount(totalCost, tipPercentage);
+        //than
+        Assertions.assertEquals(80, result);
+    }
+
+    @Test
+    void ninePercentShouldReturnNine() {
+        //given
+        int[] totalCost = {25, 25, 25, 25};
+        int tipPercentage = 9;
+        //when
+        int result = TipCalculator.calculateTipAmount(totalCost, tipPercentage);
+        //than
+        Assertions.assertEquals(-1, result);
+    }
+
+    @Test
+    void eightyOnePercentShouldReturnEightyOne() {
+        //given
+        int[] totalCost = {25, 25, 25, 25};
+        int tipPercentage = 81;
+        //when
+        int result = TipCalculator.calculateTipAmount(totalCost, tipPercentage);
+        //than
+        Assertions.assertEquals(-1, result);
+    }
+
+    @Test
+    void shouldEightyReturnTrue() {
+        boolean result = TipCalculator.isTipValid(80);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void shouldTenReturnTrue() {
+        boolean result = TipCalculator.isTipValid(10);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void shouldNineReturnFalse() {
+        boolean result = TipCalculator.isTipValid(9);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void shouldEightyOneReturnFalse() {
+        boolean result = TipCalculator.isTipValid(81);
+        Assertions.assertFalse(result);
     }
 }
-
 
