8994c32d4c184aa533a1e024160b78f13621f265
CsoreTamas <csore.tamas123@gmail.com>
2025-03-13 10:58:18 +0100

    strange-polynomial-sum

---
+++ strange-polynomial-sum/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.exit(0);
+        }
+        int result = 0;
+        for (int i = 1; i <= number; i++) {
+            if (i % 4 == 2) {
+                result += (int) Math.pow(i, 2);
+            } else if (i % 4 == 3) {
+                result += (int) Math.pow(i, 3);
+            } else if (i % 4 == 0) {
+                result += (int) Math.pow(i, 4);
+            } else {
+                result += i;
+            }
+        }
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
