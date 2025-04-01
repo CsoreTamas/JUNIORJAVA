567707bd281bf0e6bfcd6c6f24ac909895bef11b
CsoreTamas <csore.tamas123@gmail.com>
2025-04-01 17:32:45 +0200

    Methods practice homework

---
+++ methods-practice-2/src/MethodPractice.java
@@ -0,0 +1,148 @@
+public class MethodPractice {
+    public static void main(String[] args) {
+
+    }
+
+    // 1. Writes your own name to the console
+    public static void printMyOwnName() {
+        System.out.println("Csőre Tamás");
+    }
+
+    // 2. Writes a welcome message to the console
+    public static void printAWelcomeMessage() {
+        System.out.println("Welcome in this perfect practice");
+    }
+
+    // 3. Gets in a number and returns its 30%
+    public static int getNumber30Percent(int number) {
+        return number * 30 / 100;
+    }
+
+    // 4. Gets in an array of Strings and returns an empty String
+    public static String getAnEmtyString(String[] stings) {
+        return "";
+    }
+
+    // 5. Gets in a number and returns its 42.8%
+    public static double getANumber42Point8Percent(int number) {
+        return number * 42.8 / 100;
+    }
+
+    // 6. Gets in a String and returns the double of its length
+    public static int multiplyTheStringLengthByTwo(String string) {
+        return string.length() * 2;
+    }
+
+    // 7. Gets in two numbers and returns how much you would need to add to the first number to get the second one
+    public static int getTheDifference(int first, int second) {
+        return second - first;
+    }
+
+    // 8. Gets in a String and returns the square root of its length
+    public static double getTheStringLengthSquareRoot(String string) {
+        return Math.sqrt(string.length());
+    }
+
+    // 9. Gets in a String and returns the factorial of its length
+    public static int getTheStringLengthFactorial(String string) {
+        int result = 1;
+        for (int i = 1; i <= string.length(); i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // 10. Gets in a number and returns 100 divided by that number
+    public static double division100WithNumber(int number) {
+        return (double) 100 / number;
+    }
+
+    // 11. Gets in a number and …
+    // a. returns the square of it if it is prime
+    // b. returns the double of it if it is not a prime and is divisible by 4
+    // c. returns the triple of it if it is not a prime and is not divisible by 4
+    public static int getANumberIf(int number) {
+        if (isPrime(number)) {
+            return number * number;
+        } else if (number % 4 == 0) {
+            return number * 2;
+        } else {
+            return number * 3;
+        }
+    }
+
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 12. Gets in an array of Strings and returns a new array of numbers of each String’s length
+    public static int[] getTheStringsLength(String[] array) {
+        int[] resultArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            resultArray[i] = array[i].length();
+        }
+        return resultArray;
+    }
+
+    // 13.Gets in a gross salary and calculates the net salary according to the Hungarian rules in the following way:
+// a. deduces 15% as SZJA (personal income tax) and 18.5% as TBJ (social security tax)
+// b. the remaining amount is going to be the net salary
+// c. So if someone earns 100 000 HUF gross per month, their net salary is going to be 66500 HUF
+// d. Use separate methods to calculate SZJA and TBJ.
+    public static double calculateNetSalary(double number) {
+        return (number - (calculatePersonalIncomeTax(number) + calculateSocialSecurityTax(number)));
+    }
+
+    public static double calculatePersonalIncomeTax(double number) {
+        return number * 15 / 100;
+    }
+
+    public static double calculateSocialSecurityTax(double number) {
+        return number * 18.5 / 100;
+    }
+
+    // 14. Gets in an array of booleans and returns a1 XOR a2 XOR a3 … XOR an
+    public static boolean decideTrueOrFalse(boolean[] array) {
+        boolean result = false;
+        for (boolean trueFalse : array) {
+            result |= trueFalse;
+        }
+        return result;
+    }
+
+    // 15.Gets in an array of numbers and returns a new array with all the negative numbers in from the original array
+    public static int[] filWithNumbersUnder0(int[] array) {
+        int[] resultArray = new int[countElements(array)];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (isSmallerThan0(array[i])) {
+                resultArray[j] = array[i];
+                j++;
+            }
+        }
+        return resultArray;
+    }
+
+    public static boolean isSmallerThan0(int number) {
+        return number < 0;
+    }
+
+    public static int countElements(int[] array) {
+        int counter = 0;
+        for (int number : array) {
+            if (isSmallerThan0(number)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
---
+++ methods-practice-2/src/MethodPracticeTest.java
@@ -0,0 +1,174 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+public class MethodPracticeTest {
+    @ParameterizedTest
+    @CsvSource({"-100, -30", "100 , 30", "200,60 "})
+    void shouldReturnNumber30Percent(int number, int expected) {
+        //given when
+        int result = MethodPractice.getNumber30Percent(number);
+        //then
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 4.
+    @Test
+    void shouldReturnAnEmptyString() {
+        //given
+        String[] strings = {"Hello, Welcome, Good by"};
+        //when
+        String result = MethodPractice.getAnEmtyString(strings);
+        //then
+        Assertions.assertEquals("", result);
+    }
+
+    // 5.
+    @ParameterizedTest
+    @CsvSource({"100, 42.8", "200, 85.6"})
+    void shouldReturnNumber42Point8Percent(int number, double expected) {
+        double result = MethodPractice.getANumber42Point8Percent(number);
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 6.
+    @ParameterizedTest
+    @CsvSource({"Hello, 10", "1, 2"})
+    void shouldGetStringsLengthMultiplyByTwo(String strings, int expected) {
+        int result = MethodPractice.multiplyTheStringLengthByTwo(strings);
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 7.
+    @ParameterizedTest
+    @CsvSource({"10,20,10", "0,0,0", "-5,5,10"})
+    void shouldGetTheDifferenceOf2Number(int secondNumber, int firstNumber, int expected) {
+        int result = MethodPractice.getTheDifference(secondNumber, firstNumber);
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 8.
+    @ParameterizedTest
+    @CsvSource({"HelloHell, 3"})
+    void shouldGetTheStringLengthSquareRoot(String string, double expected) {
+        double result = MethodPractice.getTheStringLengthSquareRoot(string);
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 9.
+    @ParameterizedTest
+    @CsvSource({"World, 120", "good, 24"})
+    void shouldGetTheStringLengthFactorial(String string, int expected) {
+        int result = MethodPractice.getTheStringLengthFactorial(string);
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 10.
+    @ParameterizedTest
+    @CsvSource({"100,1", "40,2.5", "-50,-2"})
+    void shouldGetNumberDividedBy100(int number, double expected) {
+        double result = MethodPractice.division100WithNumber(number);
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 11.
+    @ParameterizedTest
+    @CsvSource({"3,9", "4,8", "21,63", "-4,-8"})
+    void shouldGetCorrectNumber(int number, int expected) {
+        int result = MethodPractice.getANumberIf(number);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6})
+    void shouldDecideIsPrimeOrNot(int number) {
+        boolean result = MethodPractice.isPrime(number);
+        Assertions.assertFalse(result);
+    }
+
+    // 12.
+    @Test
+    void shouldReturnStringsLengthInAnArray() {
+        String[] strings = {"Hello", "Good", "Bad", "Awesome"};
+        int[] length = {5, 4, 3, 7};
+        int[] result = MethodPractice.getTheStringsLength(strings);
+        Assertions.assertArrayEquals(length, result);
+    }
+
+    // 13.
+    @ParameterizedTest
+    @CsvSource({"100,15", "200,30"})
+    void shouldReturn15Percent(double number, double expected) {
+        double result = MethodPractice.calculatePersonalIncomeTax(number);
+        Assertions.assertEquals(expected, result);
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({"100,18.5"})
+    void shouldReturn18Point5Percent(double number, double expected) {
+        double result = MethodPractice.calculateSocialSecurityTax(number);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"100_000,66500"})
+    void shouldReturnHungarianNetSalary(double number, double expected) {
+        double result = MethodPractice.calculateNetSalary(number);
+        Assertions.assertEquals(expected, result);
+    }
+
+    // 14.
+    @ParameterizedTest
+    @MethodSource("getTrue")
+    void shouldTurnTrue(boolean[] array) {
+        boolean result = MethodPractice.decideTrueOrFalse(array);
+        Assertions.assertTrue(result);
+    }
+
+    public static Stream<Arguments> getTrue() {
+        return Stream.of(
+                Arguments.of(new boolean[]{true, true, false, true, false}, true),
+                Arguments.of(new boolean[]{false, false, true, false, true, true}, true)
+        );
+    }
+    // If I change the }, true) to false nothing changes, but it works, I think. This point is yours. :D
+
+    // 15.
+    @ParameterizedTest
+    @MethodSource("fillTheArray")
+    void shouldFillTheArrayWithNumbersUnder0(int[] array, int[] expected) {
+        int[] result = MethodPractice.filWithNumbersUnder0(array);
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    public static Stream<Arguments> fillTheArray() {
+        return Stream.of(
+                Arguments.of(new int[]{-1, -3, -100, 5, 6, 4, -9}, new int[]{-1, -3, -100, -9}),
+                Arguments.of(new int[]{-1, 3, 100, 5, 6, -4, 9}, new int[]{-1, -4})
+        );
+        // I fill like I'm too small to use this MethSource, but thank you soo much for explanation. :)
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-3, -2, -1, 0, 1, 2, 3})
+    void shouldDecideIsSmallerThan0(int number) {
+        boolean result = MethodPractice.isSmallerThan0(number);
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void shouldCountNumbersWhichSmallerThan0() {
+        int[] array = {-1, -2, -3, 0, 1, 2, 3};
+        int result = MethodPractice.countElements(array);
+        Assertions.assertEquals(3, result);
+    }
+}
+
