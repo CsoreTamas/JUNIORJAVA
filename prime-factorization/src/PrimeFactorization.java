21719b651804f4c57dad80c4e4f0c8c7d545df1d
CsoreTamas <csore.tamas123@gmail.com>
2025-04-16 19:17:32 +0200

    Prime factorization homawork

---
+++ prime-factorization/src/PrimeFactorization.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactorization {
+    public static void main(String[] args) {
+        System.out.println(primes(100));
+    }
+
+    public static List<Integer> getPrimes = new ArrayList<>();
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void isDivisibleByTwo(int number) {
+        while (number % 2 == 0) {
+            getPrimes.add(2);
+            number /= 2;
+        }
+    }
+
+    public static void isDivisibleByOddNumbers(int number) {
+        for (int i = 3; i <= number; i += 2) {
+            while (number % i == 0) {
+                getPrimes.add(i);
+                number /= i;
+            }
+        }
+    }
+
+    public static void isGreaterThanTwoPrime(int number) {
+        if (number > 2 && isPrime(number)) {
+            getPrimes.add(number);
+        }
+    }
+
+    public static List<Integer> primes (int number){
+        isDivisibleByTwo(number);
+        isDivisibleByOddNumbers(number);
+        isGreaterThanTwoPrime(number);
+        return getPrimes;
+    }
+
+    public static void clearPrimes() {
+        getPrimes.clear();
+    }
+}
+
---
+++ prime-factorization/src/PrimeFactorizationTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PrimeFactorizationTest {
+    @ParameterizedTest
+    @ValueSource(ints = {2, 3, 5, 7, 11})
+    void shouldBeTrueCauseItsPrime(int number) {
+        Assertions.assertTrue(PrimeFactorization.isPrime(number));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {4, 0, 6,1})
+    void shouldBeFalseCauseItsNotPrime(int number) {
+        Assertions.assertFalse(PrimeFactorization.isPrime(number));
+    }
+
+    @Test
+    void shouldDivisibleByTwo(){
+        PrimeFactorization.isDivisibleByTwo(8);
+        Assertions.assertEquals(3,PrimeFactorization.getPrimes.size());
+    }
+
+    @Test
+    void shouldDivisibleByOddNumbers(){
+        PrimeFactorization.clearPrimes();
+        PrimeFactorization.isDivisibleByOddNumbers(15);
+        Assertions.assertEquals( 2,PrimeFactorization.getPrimes.size());
+    }
+
+    @Test
+    void shouldBeGreaterThenTwoAndPrime(){
+        PrimeFactorization.clearPrimes();
+        PrimeFactorization.isGreaterThanTwoPrime(11);
+        PrimeFactorization.isGreaterThanTwoPrime(3);
+        PrimeFactorization.isGreaterThanTwoPrime(113);
+        Assertions.assertEquals(3,PrimeFactorization.getPrimes.size());
+    }
+}
