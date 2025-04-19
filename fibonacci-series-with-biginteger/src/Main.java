0458f591c6bb441d4768efe65c563021d4d8a4d5
CsoreTamas <csore.tamas123@gmail.com>
2025-04-19 10:23:19 +0200

    Fibonacci series with BigInteger

--- fibonacci-series-with-biginteger/src/Main.java
+++ fibonacci-series-with-biginteger/src/Main.java
@@ -1,14 +1,17 @@
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class Main {
+
 
     public static int calculateFibonacciValue(int number) {
         if (number == 0) {
             return 0;
-        }if (number == 1){
+        }
+        if (number == 1) {
             return 1;
         }
-        return calculateFibonacciValue(number - 1) + calculateFibonacciValue(number -2);
+        return calculateFibonacciValue(number - 1) + calculateFibonacciValue(number - 2);
     }
 
 
@@ -24,7 +27,7 @@ public class Main {
         if (number.compareTo(BigInteger.ZERO) == 0) {
             return zero;
         }
-        // == 0 means the number and the BigInteger.ONE(meaning 1) are equals!!
+        // == 0 means the number and the BigInteger.ONE(meaning 1) should be equal to
         if (number.compareTo(BigInteger.ONE) == 0) {
             return one;
         }
@@ -40,8 +43,37 @@ public class Main {
         return calculateFibonacciValue(number.subtract(BigInteger.ONE)).add(calculateFibonacciValue(number.subtract(BigInteger.TWO)));
     }
 
-    public static void main(String[] args) {
-        System.out.println(calculateFibonacciValue(BigInteger.TEN));
+    static BigInteger[] array = new BigInteger[1000];
 
+    public static BigInteger calculateFibonacciValueShortCut(BigInteger number) {
+        if (number.compareTo(BigInteger.TWO) < 1) {
+            return BigInteger.valueOf(1);
+        }
+
+        BigInteger previous;
+        BigInteger previousPrevious;
+
+        //.intValue() for the indexes!!
+        if (!array[number.subtract(BigInteger.ONE).intValue()].equals(BigInteger.ZERO)) {
+            previous = array[number.subtract(BigInteger.ONE).intValue()];
+        } else {
+            previous = calculateFibonacciValueShortCut(number.subtract(BigInteger.ONE));
+            array[number.subtract(BigInteger.ONE).intValue()] = previous;
+        }
+        if (!array[number.subtract(BigInteger.TWO).intValue()].equals(BigInteger.ZERO)) {
+            previousPrevious = array[number.subtract(BigInteger.TWO).intValue()];
+        } else {
+            previousPrevious = calculateFibonacciValueShortCut(number.subtract(BigInteger.TWO));
+            array[number.subtract(BigInteger.TWO).intValue()] = previousPrevious;
+        }
+        return previousPrevious.add(previous);
+    }
+
+
+    public static void main(String[] args) {
+        //If the BigInteger[] array is empty, its indexes are null. We have to fill it with BigInteger.ZERO to run the code.
+        //If we don't fill the array with ZERO, we would have to calculate with null, which doesn't work.
+        Arrays.fill(array, BigInteger.ZERO);
+        System.out.println(calculateFibonacciValueShortCut(BigInteger.valueOf(100)));
     }
 }
