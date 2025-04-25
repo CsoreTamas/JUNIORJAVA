37fc2b7c49f7babfd686eb1146d425fd219299e0
CsoreTamas <csore.tamas123@gmail.com>
2025-04-25 10:25:32 +0200

    String small exercises

--- string-small-exercises/src/StringSmallExercisesTest.java
+++ string-small-exercises/src/StringSmallExercisesTest.java
@@ -6,11 +6,30 @@ public class StringSmallExercisesTest {
     void shouldCountTheBInTheString() {
         String testString = "BjkshdsaléBdkjshakldasdhasgB";
         Assertions.assertEquals(3, StringSmallExercises.countTheBOccurrenceInString(testString));
+        Assertions.assertEquals(1, StringSmallExercises.countTheBOccurrenceInString("Basd"));
+        Assertions.assertEquals(1, StringSmallExercises.countTheBOccurrenceInString("asdB"));
+        Assertions.assertEquals(3, StringSmallExercises.countTheBOccurrenceInString("BBB"));
+        Assertions.assertEquals(1, StringSmallExercises.countTheBOccurrenceInString("B"));
+        Assertions.assertEquals(0, StringSmallExercises.countTheBOccurrenceInString(""));
+        Assertions.assertEquals(0, StringSmallExercises.countTheBOccurrenceInString("b"));
     }
 
     @Test
     void shouldCountTheHelloInAString() {
+        String start = "helloasdasd";
+        String end = "asdasdhello";
         String testString = "hellohello";
+        String moreHellos = "hsadhellohasdhellosdhellodsa";
+        String hello = "hello";
+        String smallerThan5 = "asdf";
+        String empty = "";
+
+        Assertions.assertEquals(1, StringSmallExercises.countHelloInAString(start));
+        Assertions.assertEquals(1, StringSmallExercises.countHelloInAString(end));
         Assertions.assertEquals(2, StringSmallExercises.countHelloInAString(testString));
+        Assertions.assertEquals(3, StringSmallExercises.countHelloInAString(moreHellos));
+        Assertions.assertEquals(1, StringSmallExercises.countHelloInAString(hello));
+        Assertions.assertEquals(0, StringSmallExercises.countHelloInAString(smallerThan5));
+        Assertions.assertEquals(0, StringSmallExercises.countHelloInAString(empty));
     }
 }
