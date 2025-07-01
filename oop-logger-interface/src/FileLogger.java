8b7fbd05ae9d672115a8dfda86e38ace874b31d8
CsoreTamas <csore.tamas123@gmail.com>
2025-07-01 08:42:57 +0200

    oop logger homework

--- oop-logger-interface/src/FileLogger.java
+++ oop-logger-interface/src/FileLogger.java
@@ -9,11 +9,9 @@ public class FileLogger implements Logger<String> {
     }
 
     @Override
-    public void log(String message) throws IOException {
-        try {
-            FileWriter fileWriter = new FileWriter(fileName, true);
+    public void log(String message) {
+        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
             fileWriter.write(message + "\n");
-            fileWriter.close();
             System.out.println("Successful write to file: " + message);
         } catch (IOException exception) {
             System.out.println("Failed to write to file!");
