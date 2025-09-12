de3f3b2afa774cabfc9bfb1865b5d708ce87b05b
CsoreTamas <csore.tamas123@gmail.com>
2025-09-12 10:55:41 +0200

    UserComment class repair

--- csv-form-input-validator-gradle/src/main/java/org/example/usercomment/UserComment.java
+++ csv-form-input-validator-gradle/src/main/java/org/example/usercomment/UserComment.java
@@ -1,21 +1,10 @@
 package org.example.usercomment;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class UserComment {
     private String name;
     private String email;
     private String comment;
-
-    @Override
-    public String toString() {
-        return String.format("Name: %s %nEmail: %s %nComment: %s %n", name, email, comment);
-    }
 }
