19dc43d2f1fcee30b4ada5b4d371b94785b52f72
CsoreTamas <csore.tamas123@gmail.com>
2025-09-13 10:25:36 +0200

    add an assertNull to parser test

--- cities-and-teperatures-maven/src/test/java/CsVParserTest.java
+++ cities-and-teperatures-maven/src/test/java/CsVParserTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class CsVParserTest {
     @Test
     void shouldReadCsVFiles() {
         List<City> cities = CsvParser.csvReader("src/test/java/test.csv");
+
+        assertNotNull(cities);
 
         assertEquals("Budapest", cities.getFirst().getName());
         assertEquals(22, cities.getFirst().getTemperature());
